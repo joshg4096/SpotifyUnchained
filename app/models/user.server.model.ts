eba4365b44ec624a9b937717a5c990771fa4d546
Joshua Goldberg <joshg4096@gmail.com>
2020-08-20 20:21:56 -0400

    Use typegoose to define user model

--- app/models/user.server.model.ts
+++ app/models/user.server.model.ts
@@ -3,144 +3,167 @@
 /**
  * Module dependencies.
  */
-var mongoose = require('mongoose'),
-	Schema = mongoose.Schema,
-	crypto = require('crypto');
+import crypto from 'crypto'
+import { prop, pre, ReturnModelType, DocumentType } from '@typegoose/typegoose';
+import validator from 'validator';
 
 /**
  * A Validation function for local strategy properties
  */
-var validateLocalStrategyProperty = function(property) {
+var validateLocalStrategyProperty = function (this, property) {
 	return ((this.provider !== 'local' && !this.updated) || property.length);
 };
 
 /**
  * A Validation function for local strategy password
  */
-var validateLocalStrategyPassword = function(password) {
+var validateLocalStrategyPassword = function (this, password) {
 	return (this.provider !== 'local' || (password && password.length > 6));
 };
 
 /**
- * User Schema
+ * A Validation function for local strategy email
  */
-var UserSchema = new Schema({
-	firstName: {
-		type: String,
-		trim: true,
-		default: '',
-		validate: [validateLocalStrategyProperty, 'Please fill in your first name']
-	},
-	lastName: {
-		type: String,
-		trim: true,
-		default: '',
-		validate: [validateLocalStrategyProperty, 'Please fill in your last name']
-	},
-	displayName: {
-		type: String,
-		trim: true
-	},
-	email: {
-		type: String,
-		trim: true,
-		default: '',
-		validate: [validateLocalStrategyProperty, 'Please fill in your email'],
-		match: [/.+\@.+\..+/, 'Please fill a valid email address']
-	},
-	username: {
-		type: String,
-		unique: 'testing error message',
-		required: 'Please fill in a username',
-		trim: true
-	},
-	password: {
-		type: String,
-		default: '',
-		validate: [validateLocalStrategyPassword, 'Password should be longer']
-	},
-	salt: {
-		type: String
-	},
-	provider: {
-		type: String,
-		required: 'Provider is required'
-	},
-	providerData: {},
-	additionalProvidersData: {},
-	roles: {
-		type: [{
-			type: String,
-			enum: ['user', 'admin']
-		}],
-		default: ['user']
-	},
-	updated: {
-		type: Date
-	},
-	created: {
-		type: Date,
-		default: Date.now
-	},
-	/* For reset password */
-	resetPasswordToken: {
-		type: String
-	},
-	resetPasswordExpires: {
-		type: Date
-	}
-});
+var validateLocalStrategyEmail = function (this, email) {
+	return (this.provider !== 'local' || (email && validator.isEmail(email)));
+};
+
+class ProviderData {
+	@prop()
+	public accessToken?: string;
+
+	@prop()
+	public refreshToken?: string;
+}
+
+enum Role {
+	USER = 'user',
+	ADMIN = 'admin'
+}
 
 /**
  * Hook a pre save method to hash the password
  */
-UserSchema.pre('save', function(next) {
+@pre<User>('save', function (next) {
 	if (this.password && this.password.length > 6) {
-		this.salt = Buffer.from(crypto.randomBytes(16).toString('base64'), 'base64');
+		this.salt = Buffer.from(crypto.randomBytes(16).toString('base64'), 'base64').toString();
 		this.password = this.hashPassword(this.password);
 	}
 
 	next();
-});
-
-/**
- * Create instance method for hashing a password
- */
-UserSchema.methods.hashPassword = function(password) {
-	if (this.salt && password) {
-		return crypto.pbkdf2Sync(password, this.salt, 10000, 64, 'sha256').toString('base64');
-	} else {
-		return password;
-	}
-};
-
-/**
- * Create instance method for authenticating user
- */
-UserSchema.methods.authenticate = function(password) {
-	return this.password === this.hashPassword(password);
-};
-
-/**
- * Find possible not used username
- */
-UserSchema.statics.findUniqueUsername = function(username, suffix, callback) {
-	var _this = this;
-	var possibleUsername = username + (suffix || '');
-
-	_this.findOne({
-		username: possibleUsername
-	}, function(err, user) {
-		if (!err) {
-			if (!user) {
-				callback(possibleUsername);
-			} else {
-				return _this.findUniqueUsername(username, (suffix || 0) + 1, callback);
-			}
-		} else {
-			callback(null);
+})
+export class User {
+	@prop({
+		default: '',
+		trim: true,
+		validate: {
+			validator: validateLocalStrategyProperty,
+			message: 'Please fill in your first name'
 		}
-	});
-};
+	})
+	public firstName?: string;
 
-mongoose.model('User', UserSchema);
+	@prop({
+		default: '',
+		trim: true,
+		validate: {
+			validator: validateLocalStrategyProperty,
+			message: 'Please fill in your last name'
+		}
+	})
+	public lastName?: string;
+
+	@prop({ trim: true })
+	public displayName?: string;
+
+	@prop({
+		default: '',
+		trim: true,
+		validate: {
+			validator: validateLocalStrategyEmail,
+			message: 'Please fill in your email'
+		}
+	})
+	public email?: string;
+
+	@prop({ required: true, unique: true, trim: true })
+	public username!: string;
+
+	@prop({
+		default: '',
+		trim: true,
+		validate: {
+			validator: validateLocalStrategyPassword,
+			message: 'Password should be longer'
+		}
+	})
+	public password?: string;
+
+	@prop()
+	public salt?: string;
+
+	@prop({ required: true })
+	public provider!: string;
+
+	@prop()
+	public providerData?: ProviderData;
+
+	@prop()
+	public additionalProvidersData?: any;
+
+	@prop({ enum: Role, default: Role.USER })
+	public roles?: Role
+
+	@prop()
+	public updated?: Date;
+
+	@prop({ default: Date.now })
+	public created?: Date;
+
+	@prop()
+	public resetPasswordToken?: string;
+
+	@prop()
+	public resetPasswordExpires?: string;
+
+	/**
+	 * Create instance method for hashing a password
+	 */
+	public hashPassword(this: DocumentType<User>, password: string) {
+		if (this.salt && password) {
+			return crypto.pbkdf2Sync(password, this.salt, 10000, 64, 'sha256').toString('base64');
+		} else {
+			return password;
+		}
+	}
+
+	/**
+	 * Create instance method for authenticating user
+	 */
+	public authenticate(this: DocumentType<User>, password: string) {
+		return this.password === this.hashPassword(password);
+	}
+
+	/**
+	 * Find possible not used username
+	 */
+	public static findUniqueUsername(this: ReturnModelType<typeof User>, username: string, suffix: number, callback) {
+		var _this = this;
+		var possibleUsername = username + (suffix || '');
+
+		_this.findOne({
+			username: possibleUsername
+		})
+			.then(user => {
+				if (!user) {
+					callback(possibleUsername);
+				} else {
+					return _this.findUniqueUsername(username, (suffix || 0) + 1, callback);
+				}
+			})
+			.catch(err => {
+				console.log(err)
+				callback(null)
+			});
+	}
+}
